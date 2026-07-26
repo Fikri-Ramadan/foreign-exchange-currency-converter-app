01d02597f565cc238655c65f9f537cbac0530f96
Fikri Ramadan <fikrinr.tech@gmail.com>
2026-07-26 12:56:07 +0700

    Setup state management and app structure
    Add zustand as state management solution and scaffold initial app structure:
    - Install zustand dependency
    - Create CurrencyLog store with bear state (test store)
    - Define custom color palette CSS variables
    - Clean up boilerplate home page

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useBear } from "@/stores/CurrencyLog";
+
+export default function Counter() {
+  const { bears } = useBear();
+  const { increasePopulation, removeAllBears } = useBear();
+  const handleIncreasePopulation = () => increasePopulation();
+  const handleRemoveBears = () => removeAllBears();
+  return (
+    <div>
+      <div className="text-2xl text-white bg-neutral-900">Counter: {bears}</div>
+      <button className="w-20 bg-gray-700 text-center" onClick={handleIncreasePopulation}>+</button>
+      <div onClick={handleRemoveBears}>reset</div>
+    </div>
+  );
+}
