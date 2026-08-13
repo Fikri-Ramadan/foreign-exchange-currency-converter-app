106b51a9c55c157d7620cedd385c2388fac60764
Fikri Ramadan <fikrinr.tech@gmail.com>
2026-08-13 13:33:04 +0700

    Add hooks: useExchangeRate and useHasHydrated
    Introduce two new hooks. useExchangeRate uses SWR to fetch exchange rates from api.frankfurter.dev, updates ConverterStore (sets rate, computes receive amount, toggles swapping), and configures deduping/revalidate options with error handling; it also contains a commented alternative using SWRMutation. useHasHydrated provides a simple hydration flag (hasHydrated) set via useEffect for client-only rendering checks.

---
+++ src/hooks/useExchangeRate.tsx
@@ -0,0 +1,68 @@
+import { fetcher } from "@/lib/utils";
+import { useConverter } from "@/stores/ConverterStore";
+import useSWR from "swr";
+
+export default function useExchangeRate() {
+  const { send: { code: base }, receive: { code: quote }, sendAmount, isSwapping, setRate, setReceiveAmount, setSwapping } = useConverter();
+
+  const { data, isValidating, isLoading, error, mutate } = useSWR(
+    `https://api.frankfurter.dev/v2/rate/${base}/${quote}`,
+    fetcher,
+    {
+      onSuccess: (data) => {
+        if (data?.rate) {
+          setRate(data.rate);
+          if (isSwapping) {
+            setSwapping(false)
+          } else {
+            const latestReceiveAmount = ((sendAmount ?? 0) * data.rate);
+            setReceiveAmount(Number(latestReceiveAmount.toFixed(4)));
+          }
+        }
+      },
+      onError: (err) => {
+        setSwapping(false);
+        console.error('Failed to fetch:', err);
+      },
+      revalidateOnFocus: false,
+      dedupingInterval: 2000,
+    }
+  );
+
+  return {
+    data,
+    isValidating,
+    isLoading,
+    isError: error,
+    mutate
+  };
+}
+
+// export default function useExchangeRate() {
+//   const { send: { code: base }, receive: { code: quote }, sendAmount, setRate, setReceiveAmount } = useConverter();
+
+//   const { data, trigger, isMutating, error } = useSWRMutation(
+//     'EXCHANGE_RATE_API',
+//     sendRequest,
+//     {
+//       onSuccess: (data) => {
+//         data?.rate && setRate(data.rate);
+//         if (data?.rate) {
+//           setRate(data.rate);
+//           setReceiveAmount((sendAmount ?? 0) * data.rate);
+//         }
+//       }
+//     }
+//   );
+
+//   const safeTrigger = (arg?: { base: string; quote: string; }) => {
+//     return trigger(arg || { base, quote });
+//   };
+
+//   return {
+//     data,
+//     trigger: safeTrigger,
+//     isMutating,
+//     isError: error
+//   };
+// }
